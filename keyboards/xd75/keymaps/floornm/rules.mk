501b4503606fbe868878ea7e65c46fc141f811ec
floor667 <lwrncbrwn@gmail.com>
2018-11-25 12:01:37 +0000

    locking and no-midi maps

---
+++ keyboards/xd75/keymaps/floornm/rules.mk
@@ -0,0 +1,14 @@
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+# Build options
+BACKLIGHT_ENABLE = no		# Switch LEDs
+MOUSEKEY_ENABLE = yes		# Emulates mouse key using keypresses
+RGBLIGHT_ENABLE = yes 		
+TAP_DANCE_ENABLE = yes 		# Use multi-tap features
+AUDIO_ENABLE = no  			# Audio stuff
+UNICODE_ENABLE = yes
+EXTRAKEY_ENABLE = no 	# Audio control and System control
+BOOTMAGIC_ENABLE = no
