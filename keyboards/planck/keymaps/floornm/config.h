b6c83a2fc16fedc8de8e8d6012c94101baf81dd2
floor667 <lwrncbrwn@gmail.com>
2018-11-25 12:01:26 +0000

    locking and no-midi maps

--- keyboards/planck/keymaps/floornm/config.h
+++ keyboards/planck/keymaps/floornm/config.h
@@ -67,10 +67,10 @@
    - Virtual sustain, portamento, and modulation wheel
    - etc.
 */
-#define MIDI_ADVANCED
+//#define MIDI_ADVANCED
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-#define MIDI_TONE_KEYCODE_OCTAVES 6
+//#define MIDI_TONE_KEYCODE_OCTAVES 6
 
 #define MOUSEKEY_DELAY             160
 #define MOUSEKEY_INTERVAL          50
