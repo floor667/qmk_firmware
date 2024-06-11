7348c64b92eea3a4c7d2c3d8f2045c92b30c7b86
floor667 <lwrncbrwn@gmail.com>
2024-06-11 21:42:57 +0100

    updbt

--- keyboards/xiudi/xd75/keymaps/floormix/keymap.c
+++ keyboards/xiudi/xd75/keymaps/floormix/keymap.c
@@ -46,15 +46,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_0, KC_MINS,  KC_EQUAL,    KC_F7,    KC_F8,    KC_F9,    KC_F10, KC_F11, KC_F12, },
   { KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_7,  KC_8, KC_9, KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCOLON,  KC_BSPC, KC_BSLS  },
   { KC_A,  KC_R,  KC_S,  KC_T,  KC_D,   KC_4,    KC_5,  KC_6, KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, KC_ENT  },
-  { KC_CAPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_1,   KC_2   KC_3, KC_K,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_GRV  },
+  { KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_1,   KC_2   KC_3, KC_K,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_GRV  },
   { MOUS, KC_RCTL, MOV, KC_NO, KC_NO, KC_NO, S(KC_SPC), KC_NO, KC_NO, KC_NO, MODF, KC_LALT, KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 
  [_MOV] = { /* FUNCTION */
   { KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6, _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_INS,    KC_F12 },
-  { _______, _______,  SC_VIW, SC_VIQ,   _______, _______, _______, DBL_PAR, KC_NO, KC_NO, KC_NO,  KC_UPHO,  KC_DEL, DBL_GRV  },
+  { _______, _______,  SC_VIW, SC_VIQ,   _______, _______, _______, DBL_PAR, KC_ESC, KC_NO, KC_NO,  KC_UPHO,  KC_DEL, DBL_GRV  },
   { _______, _______, _______, _______, _______, _______, _______, DBL_SQR,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_DNEN, DBL_QUO, _______ },
-  { _______, _______, _______, _______, KC_NO, KC_NO, _______, DBL_ANG,  KC_LBRC,  KC_VOLD, KC_VOLU,  KC_NO,  KC_RBRC,  _______, KC_CAPS },
+  { _______, _______, _______, _______, KC_NO, KC_NO, _______, DBL_ANG,  KC_TAB,  KC_VOLD, KC_VOLU,  KC_LBRC,  KC_RBRC,  _______, KC_CAPS },
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  },
  },
 
